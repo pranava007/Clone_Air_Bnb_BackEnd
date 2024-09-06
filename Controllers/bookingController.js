860612e6368780eada5c1ed787d187c1bce62097
pranava007 <pranavamuthu000@gmail.com>
2024-09-06 13:24:08 +0530

    remove

--- Controllers/bookingController.js
+++ Controllers/bookingController.js
@@ -70,7 +70,7 @@ export const updateBookingStatus = async (req, res) => {
 export const getBookings = async (req, res) => {
     try {
         const bookings = await Booking.find().populate('userId').populate('propertyId');
-        res.status(200).json({message:'seccussfully',result:bookings});
+        res.status(200).json({message:'seccussfully',bookings});
     } catch (error) {
         console.error('Error fetching bookings:', error);
         res.status(500).json({ message: 'Server error' });
